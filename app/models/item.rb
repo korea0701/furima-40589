b68219f153b66a4e4143375dba0ed8d63c52eab3
korea0701 <n.motomi0701@outlook.jp>
2024-08-27 14:10:32 +0900

    修正依頼によるコード修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,6 +15,5 @@ class Item < ApplicationRecord
   validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, presence: true, numericality: { other_than: 1, message: 'Please select' }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'must be between ¥300 and ¥9,999,999' }
   validates :price, numericality: { only_integer: true, message: 'is not a number' }
-  validates :user, presence: true
 
 end
